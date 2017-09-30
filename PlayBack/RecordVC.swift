10cfd8fb8031debc0aa50118b05cf1be35b85fd7
schlaBAM <brett.morrison@live.com>
2017-09-29 20:57:17 -0700

    recordVC layout set up

---
+++ PlayBack/RecordVC.swift
@@ -0,0 +1,36 @@
+//
+//  RecordVC.swift
+//  PlayBack
+//
+//  Created by BAM on 2017-09-29.
+//  Copyright © 2017 BAM. All rights reserved.
+//
+
+import UIKit
+
+class RecordVC: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var textLabel: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+        
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+}
