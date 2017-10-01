87f389cfb45e846ca93ae5b6eb0d710869bb8cc6
schlaBAM <brett.morrison@live.com>
2017-09-30 22:58:50 -0700

    record, playback, save audio, error handling for audioPlayer playing between VCs

--- PlayBack/RecordVC.swift
+++ PlayBack/RecordVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordVC: UIViewController {
     
@@ -15,22 +16,79 @@ class RecordVC: UIViewController {
     @IBOutlet weak var textLabel: UITextField!
     @IBOutlet weak var addButton: UIButton!
     
+    var recorder : AVAudioRecorder?
+    var player : AVAudioPlayer?
+    var url : URL?
+    let appDel = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
+        setupRecorder()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func setupRecorder(){
+        do {
+            //Create AudioRecorderSession
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+        
+        //Create AudioRecorderURL
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [path, "audio.m4a"]
+            url = NSURL.fileURL(withPathComponents: pathComponents)
+            print("URL: \(url)")
+        
+        //Create AudioRecorderSettings
+            var settings : [String : Any] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+            settings[AVSampleRateKey] = 44100
+            settings[AVNumberOfChannelsKey] = 2
+        
+        //Create AudioRecorderObj
+           try recorder = AVAudioRecorder(url: url!, settings: settings)
+            recorder!.prepareToRecord()
+            
+        } catch let error as NSError{
+            print("Error: \(error)")
+        }
     }
 
     @IBAction func recordTapped(_ sender: Any) {
-        
+        if recorder!.isRecording{
+            recorder?.stop()
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+        } else {
+            recorder?.record()
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
 
     @IBAction func playTapped(_ sender: Any) {
-        
+        do{
+            try player = AVAudioPlayer(contentsOf: url!)
+            player!.play()
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func addTapped(_ sender: Any) {
-        
+        let sound = Sound(context: appDel.persistentContainer.viewContext)
+        sound.name = textLabel.text
+        do{
+            try sound.audio = Data(contentsOf: url!)
+            appDel.saveContext()
+        } catch {
+            print(error)
+        }
+        navigationController?.popViewController(animated: true)
     }
     
 }
--- PlayBack/ViewController.swift
+++ PlayBack/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var sounds : [Sound] = []
+    var audioPlayer : AVAudioPlayer?
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        getSounds()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let player = audioPlayer {
+            if player.isPlaying{
+                player.stop()
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getSounds(){
+        do{
+            sounds = try context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            print(error)
+        }
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = sounds[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let sound = sounds[indexPath.row]
+        do {
+            try audioPlayer = AVAudioPlayer(data: sound.audio!)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            context.delete(sounds[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            getSounds()
+        }
+    }
 }
 
